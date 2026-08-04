52d21ddf268cfc380c569a91b1e03cb77dab1eb0
Software Developer <kadireprashanthreddy1@gmail.com>
2026-08-04 10:56:08 -0500

    Add: reverse-string - submission-1

---
+++ Data Structures & Algorithms/reverse-string/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length-1;
+
+        while(left < right){
+            char tempChar = s[left];
+            s[left] = s[right];
+            s[right] = tempChar;
+            left++;
+            right--;
+        }
+    }
+}
